4a9487f7e1deef0a7d622bb8df335c683b9ab6ea
unknown <nbrueske@wisc.edu>
2026-01-20 19:04:59 -0600

    added motor controlls

--- main/main.c
+++ main/main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "driver/gpio.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "soc/soc_caps.h"
@@ -16,9 +17,10 @@
 
 const static char *TAG = "EXAMPLE";
 
-/*---------------------------------------------------------------
-        ADC General Macros
----------------------------------------------------------------*/
+//Motor Setup
+#define MOTOR_IN_1 5
+#define MOTOR_IN_2 6
+
 //ADC1 Channel 3
 #define EXAMPLE_ADC1_CHAN           ADC_CHANNEL_3
 
@@ -32,11 +34,19 @@ static int voltage[10];
 #define AVG_SAMPLES_SPEED_MS 2000 // Total delay time for averaging samples
 float avg_voltage = 0;
 
+//todo -> allow user to modify target temp and allowed offset?
+float target_temp = 22.0f; // Target temperature in Celsius
+float temp_offset = 1.0; // Allowed temperature offset in Celsius
 
-static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+void init_motor_pins();
+void set_motor_state(bool state);
 
 void app_main(void)
 {
+    init_motor_pins();
+    bool motor_on = false;
+    float curr_temp = target_temp;
     //-------------ADC1 Init---------------//
     adc_oneshot_unit_handle_t adc1_handle;
     adc_oneshot_unit_init_cfg_t init_config1 = {
@@ -54,7 +64,6 @@ void app_main(void)
     //-------------ADC1 Calibration Init---------------//
     adc_cali_handle_t adc1_cali_chan_handle = NULL;
     bool do_calibration1 = adc_calibration_init(ADC_UNIT_1, EXAMPLE_ADC1_CHAN, EXAMPLE_ADC_ATTEN, &adc1_cali_chan_handle);
-
     while (1) {
         // Average out the input
         avg_voltage = 0;
@@ -66,14 +75,30 @@ void app_main(void)
         }
         avg_voltage = avg_voltage / AVG_SAMPLES;
         ESP_LOGI(TAG, "Average Cali voltage: %.2f mV", avg_voltage);
-        ESP_LOGI(TAG, "Temp: %.2f C", (avg_voltage - 500) / 10.0); // For TMP36 sensor
+        curr_temp = (avg_voltage - 543) / 10.0; // For TMP36 sensor
+        ESP_LOGI(TAG, "Temp: %.2f C", curr_temp); // For TMP36 sensor
+
+        if (curr_temp > target_temp + temp_offset) {
+            if (!motor_on) {
+                ESP_LOGI(TAG, "Temperature above target. Turning motor ON.");
+                set_motor_state(true);
+                motor_on = true;
+            }
+        } else if (curr_temp < target_temp - temp_offset) {
+            if (motor_on) {
+                ESP_LOGI(TAG, "Temperature below target. Turning motor OFF.");
+                set_motor_state(false);
+                motor_on = false;
+            }
+        }
+            
     }
 }
 
 /*---------------------------------------------------------------
         ADC Calibration
 ---------------------------------------------------------------*/
-static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
+bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
 {
     adc_cali_handle_t handle = NULL;
     esp_err_t ret = ESP_FAIL;
@@ -103,4 +128,22 @@ static bool adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_att
     }
 
     return calibrated;
+}
+
+
+void init_motor_pins() {
+    gpio_reset_pin(MOTOR_IN_1);
+    gpio_reset_pin(MOTOR_IN_2);
+    gpio_set_direction(MOTOR_IN_1, GPIO_MODE_OUTPUT);
+    gpio_set_direction(MOTOR_IN_2, GPIO_MODE_OUTPUT);
+}
+
+void set_motor_state(bool state) {
+    if (state) {
+        gpio_set_level(MOTOR_IN_1, 1);
+        gpio_set_level(MOTOR_IN_2, 0);
+    } else {
+        gpio_set_level(MOTOR_IN_1, 0);
+        gpio_set_level(MOTOR_IN_2, 0);
+    }
 }
